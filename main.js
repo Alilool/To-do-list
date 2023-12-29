52d8bde633345508de231b8e11535d84214b74b9
Alilool <120591534+Alilool@users.noreply.github.com>
2023-12-29 18:20:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+let taskInput = document.getElementById("input");
+let todoList = document.getElementById("todoList");
+let finishedList = document.getElementById("finishedTasks");
+
+function addTask() {
+    if (taskInput.value.trim() !== "") {
+        let newTask = document.createElement("li");
+        newTask.innerHTML = '<div class="li-content">' + taskInput.value + '</div>' +
+            '<div class="buttons">' +
+            '<button onclick="doneTask(this)" id="done">Done</button>' +
+            '<button onclick="removeTask(this)" id="delete">Delete</button>' +
+            '</div>';
+        todoList.appendChild(newTask);
+        taskInput.value = "";
+    }
+}
+
+function removeTask(taskElement) {
+    taskElement.parentNode.parentNode.remove();
+}
+
+function doneTask(taskElement) {
+    let newTask = document.createElement("li");
+    newTask.innerHTML = '<div class="li-content">' + taskElement.parentNode.parentNode.querySelector('.li-content').textContent + '</div>' +
+        '<div class="buttons">' +
+        '<button onclick="restore(this)" class="buttons-hover" id="restore">Restore</button>' +
+        '<button onclick="removeFinishedTask(this)" id="delete-finished">Delete</button>'
+    '</div>';
+    finishedList.appendChild(newTask);
+    removeTask(taskElement);
+}
+
+function removeFinishedTask(taskElement) {
+    taskElement.parentNode.parentNode.remove();
+}
+
+function restore(taskElement) {
+    taskElement.parentNode.parentNode.remove()
+    let newTask = document.createElement("li");
+    newTask.innerHTML = '<div class="li-content">' + taskElement.parentNode.parentNode.querySelector(".li-content").textContent + '</div>' +
+        '<div class="buttons">' +
+        '<button onclick="doneTask(this)" id="done">Done</button>' +
+        '<button onclick="removeTask(this)" id="delete">Delete</button>' +
+        '</div>';
+    todoList.appendChild(newTask);
+    removeTask(taskElement);
+}
